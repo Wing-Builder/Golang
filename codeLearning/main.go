62d9a164922a20768bfa0ebf1652e6369e50686b
MRAZAFINDRIANTSOA <marc.razafindriantsoa@ynov.com>
2023-05-16 09:26:48 +0200

    codeLearning

---
+++ codeLearning/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt" //Comment
+
+func main() {
+	var (
+		x int
+		y string
+		z bool
+	)
+	x = 29
+	y = "Alexis"
+	z = true
+	// y := 16
+
+	fmt.Printf("Mon âge est: %v!\n Mon prénom est: %v!\n J'ai plus de 18 ans: %v!", x, y, z)
+	
+}
+
+// main.main()
